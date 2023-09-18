0cd6cbdeee51a2f078deed78c61c52950895f491
fazlarabby1 <fazlarabby41@gmail.com>
2023-09-18 13:59:26 +0600

    added login page, set login route, and added recaptcha

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../Layout/MainLayout";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
+import Login from "../pages/Login/Login";
 
 export const router = createBrowserRouter([
     {
@@ -21,6 +22,14 @@ export const router = createBrowserRouter([
                 path: 'order/:category',
                 element: <Order />
             },
+            {
+                path: 'login',
+                element: <Login />
+            },
+            // {
+            //     path: 'signup',
+            //     element: <Login />
+            // },
         ]
     }
 ]);
