302a810c2ce69a78ddf75c9d88786846673130b1
fazlarabby1 <fazlarabby41@gmail.com>
2023-09-18 15:54:48 +0600

    connected with firebase, and configured firebase authentication

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import loginImg from '../../assets/others/authentication1.png';
 
 const Login = () => {
 
@@ -31,9 +32,9 @@ const Login = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
-                <div className="text-center lg:text-left md:w-1/2">
+                <div className="text-center">
                     <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <img src={loginImg} alt="Login Image" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form onSubmit={handleLogin} className="card-body">
