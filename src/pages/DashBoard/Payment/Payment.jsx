6f42056fce64c12a94a3ab496a017743486fbe8f
fazlarabby1 <fazlarabby41@gmail.com>
2023-09-26 15:26:04 +0600

    created payment form and implemented stripe payment method

--- src/pages/DashBoard/Payment/Payment.jsx
+++ src/pages/DashBoard/Payment/Payment.jsx
@@ -1,13 +1,22 @@
 import { Helmet } from "react-helmet-async";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
+// TODO: provide publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
+
     return (
-        <div>
+        <div className="w-full px-4 md:px-36">
             <Helmet>
                 <title>Bistro Boss | Payment</title>
             </Helmet>
             <SectionTitle subHeading="please provide information for" heading="Payment" />
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };
